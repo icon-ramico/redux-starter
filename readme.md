7f714a13405f94caf2f26dd536658fc253712a45
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2022-07-05 15:40:51 +0300

    Code adn readme updated
    Code adn readme updated
    
    Code adn readme updated

--- readme.md
+++ readme.md
@@ -1,5 +1,36 @@
 # Redux
 
+## Resources
+
 [video](https://www.youtube.com/watch?v=poQXNp9ItL4&t=0s)
 
 [Reference](https://github.com/ramico/MDs/tree/main/Node.js/redux)
+
+
+## Pure
+
+``` js
+// pure functions shouln't have
+/* 
+ Math.random()
+ current time
+ editubg resources db,text file
+ global var 
+*/ 
+
+// pure function
+const pure = (n, a) =>{
+    return n + a;
+}
+// imutable example
+const string1 = 'JavaScript';
+console.log(string1.toUpperCase());
+
+// mutable
+const obj = {a:5}
+obj.b = 6;
+```
+## next steps
+
+1. [solo learn](https://www.sololearn.com/learning/1097)
+1. [sign up for react hooks course for free](https://courses.webdevsimplified.com/)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import { compose, pipe } from 'lodash/fp';
+// npm i lodash.
 
 const str = '     JavaScript        ';
 const trim = str => str.trim();
 const wrapInDiv = str => `<div>${str}</div>`;
 const toLowerCase = str => str.toLowerCase();
 
-const res1 = wrapInDiv(toLowerCase(trim(str)));
+// 2 parameters
+function add(a, b) {
+  return a + b;
+}
+// HOR Highier Order Functions
+function x(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+{
+  /* <list data="aaa"> */
+}
+
+const y = x(5)(6);
+console.log('add', add(5, 6), 'y', y);
+
+const res1 = wrapInDiv(toLowerCase(trim(str))); // nesting/Object Composition
 
 const transform = compose(wrapInDiv, toLowerCase, trim);
 
@@ -15,7 +33,7 @@ const transform2 = pipe(trim, toLowerCase, wrapInDiv);
 
 const res3 = transform2(str);
 
-console.log({ res1, res2, res3 });
+console.log('example 1', { res1, res2, res3 });
 
 // const wrapInSpan = str => `<span>${str}</span>`;
 // const wrap = (type, str) => `<${type}>${str}</${type}>`;
@@ -24,3 +42,4 @@ const wrap = type => str => `<${type}>${str}</${type}>`;
 
 const transform3 = pipe(trim, toLowerCase, wrap('div'));
 const res4 = transform3(str);
+console.log({ res4 });
