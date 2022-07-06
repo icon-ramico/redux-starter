3104eba85a8174f4051d06683b1631eede085981
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2022-07-06 10:35:07 +0300

    parts

--- src/index.js
+++ src/index.js
@@ -1,47 +1,27 @@
-import { compose, pipe } from 'lodash/fp';
-// npm i lodash.
-
-console.log('-----------------------Functional programming------------------------------');
-
-const str = '     JavaScript        ';
-const trim = str => str.trim();
-const wrapInDiv = str => `<div>${str}</div>`;
-const toLowerCase = str => str.toLowerCase();
-
-// 2 parameters
-function add(a, b) {
-  return a + b;
-}
-// HOR Highier Order Functions
-function x(a) {
-  return function (b) {
-    return a + b;
-  };
-}
+/*
 {
-  /* <list data="aaa"> */
+  persons:[{},{}]
 }
+*/
 
-const y = x(5)(6);
-console.log('add', add(5, 6), 'y', y);
+const addPerson = 'ADD_PERSON'; // UPPER_SNAKE
 
-const res1 = wrapInDiv(toLowerCase(trim(str))); // nesting/Object Composition
+const addPerson_ac = person => {
+  return {
+    type: addPerson,
+    payload: person,
+  };
+};
 
-const transform = compose(wrapInDiv, toLowerCase, trim);
+const personReducer = (state = { persons: [] }, action) => {
+  switch (action.type) {
+    case addPerson:
+      return { ...state, persons: [...state.persons, action.payload] };
+    default:
+      return state;
+  }
+};
 
-const res2 = transform(str);
+import { createStore } from 'redux';
 
-const transform2 = pipe(trim, toLowerCase, wrapInDiv);
-
-const res3 = transform2(str);
-
-console.log('example 1', { res1, res2, res3 });
-
-// const wrapInSpan = str => `<span>${str}</span>`;
-// const wrap = (type, str) => `<${type}>${str}</${type}>`;
-
-const wrap = type => str => `<${type}>${str}</${type}>`;
-
-const transform3 = pipe(trim, toLowerCase, wrap('div'));
-const res4 = transform3(str);
-console.log({res4})
+const store = createStore(personReducer);
